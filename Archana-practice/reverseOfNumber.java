dc944179ceccc825af10f531e5d49c130f4b7e85
archanachowla <carchana01012004@gmail.com>
2024-12-27 21:03:18 +0530

    reverse of a number program added to the repository

---
+++ Archana-practice/reverseOfNumber.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+public class reverseOfNumber {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int num=sc.nextInt();
+        int rev=0;
+        
+        while(num>0){
+            int rem=num%10;
+            rev=rev*10+rem;
+            num/=10;
+        }
+        System.out.println(rev);
+    }
+}
