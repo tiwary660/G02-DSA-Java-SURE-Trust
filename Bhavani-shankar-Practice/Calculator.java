066866cf9342049d4a1716e9cfbc9c5ea1527e8d
Bhavani-Shankar13 <146547550+Bhavani-Shankar13@users.noreply.github.com>
2024-12-28 15:16:31 +0530

    Update and rename Calculator1.java to Calculator.java

--- Bhavani-shankar-Practice/Calculator.java
+++ Bhavani-shankar-Practice/Calculator.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class Calculator1 {
+public class Calculator {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.println("Simple Calculator");
